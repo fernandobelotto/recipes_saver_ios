c94044ec058642b4448a955114fdc3a137e7ab49
fernandobosco <fbosco@dlocal.com>
2022-01-10 20:32:03 -0300

    Initial Commit

---
+++ recipes_saver_ios/recipes_saver_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipes_saver_iosApp.swift
+//  recipes_saver_ios
+//
+//  Created by Fernando Bosco on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct recipes_saver_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
